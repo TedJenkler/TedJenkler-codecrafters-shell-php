23d183ce12eb9c7f4ba0a47fc9875419fdb528a2
Ted Jenkler <tedjenkler@gmail.com>
2024-12-20 02:00:37 +0100

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -2,18 +2,24 @@
 
 error_reporting(E_ALL);
 
-$availableCommands = [];
+function main() {
+    fwrite(STDOUT, "$ ");
 
+    $userInput = fgets(STDIN);
 
-fwrite(STDOUT, "$ ");
+    $availableCommands = [];
 
-$userInput = fgets(STDIN);
+    $userInput = rtrim($userInput, "\n");
 
-$userInput = rtrim($userInput, "\n");
+    if ($userInput) {
+        $commandArray = explode(" ", $userInput);
 
-if($userInput){
-    $commandArray = explode(" ", $userInput);
-    if($commandArray[0] && !in_array($commandArray[0], $availableCommands)) {
-        fwrite(STDOUT, $commandArray[0] . ": command not found\r\n");
+        if ($commandArray[0] && !in_array($commandArray[0], $availableCommands)) {
+            fwrite(STDOUT, $commandArray[0] . ": command not found\r\n");
+        }
     }
+
+    main();
 }
+
+main();
