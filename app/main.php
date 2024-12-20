3761007f9b7185ea10e0843dcdea5108999403a0
Ted Jenkler <tedjenkler@gmail.com>
2024-12-20 01:49:53 +0100

    codecrafters submit [skip ci]

--- app/main.php
+++ app/main.php
@@ -1,8 +1,22 @@
 <?php
+
 error_reporting(E_ALL);
 
+$availableCommands = [];
 
-fwrite(STDOUT, "$ ");
+function main() {
+    fwrite(STDOUT, "$ ");
 
-// Wait for user input
-fgets(STDIN);
+    $userInput = fgets(STDIN);
+
+    $userInput = rtrim($userInput, "\n");
+
+    if($userInput) {
+        $commandArray = explode(" ", $userInput);
+        if($commandArray[0] && !in_array($commandArray[0], $availableCommands)) {
+            fwrite(STDOUT, $commandArray[0] . ": command not found\r\n");
+        }
+    }
+}
+
+main();
